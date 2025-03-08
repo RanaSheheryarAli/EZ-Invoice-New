18f1d2898f55d32099b00d4902b7e03db7decda9
Sheheryar Ali <sherry327676@gmail.com>
2025-03-09 01:51:54 +0500

    Authentication with Node APIs And Mongo DB

--- app/src/main/java/com/example/ezinvoice/apis/SignupApi.kt
+++ app/src/main/java/com/example/ezinvoice/apis/SignupApi.kt
@@ -1,6 +1,7 @@
 package com.example.ezinvoice.apis
 
 import com.example.ezinvoice.models.AppUser
+import com.example.ezinvoice.models.LoginModel
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.Body
@@ -11,4 +12,6 @@ interface SignupApi {
     @Headers("Content-Type: application/json") // Ensure JSON format
     @POST("signup")
     fun signup(@Body user: AppUser): Call<ResponseBody>
+    @POST("signin")
+    fun signin(@Body user: LoginModel): Call<ResponseBody>
 }
