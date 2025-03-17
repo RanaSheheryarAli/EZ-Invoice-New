6476db18e770b4ae3fb35e2a2e3c2a2558b6e8e8
Sheheryar Ali <sherry327676@gmail.com>
2025-03-17 12:40:11 +0500

    testing from mohsin

--- app/src/main/java/com/example/ezinvoice/StartScreen.kt
+++ app/src/main/java/com/example/ezinvoice/StartScreen.kt
@@ -2,6 +2,7 @@ package com.example.ezinvoice
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,5 +23,7 @@ class StartScreen : AppCompatActivity() {
             val intent=Intent(this@StartScreen,Signup::class.java)
             startActivity(intent)
         }
+
+        Log.e("TAG", "onCreate: testing.......", )
     }
 }
